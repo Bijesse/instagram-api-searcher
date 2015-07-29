fe72449903b1b21b147c8bbba8fbcf84e21b58d5
bijesse <bijesse.thomas@gmail.com>
2015-07-29 13:42:38 +0000

    added solution

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-$(document).ready(function() {
-
-    // YOUR CODE GOES HERE
-});
-
+$(document).ready(
+  function() {
+    $('#search-btn').click(
+      function() {
+        searchInstagram(
+          $('#search-query').val(),
+          8,
+          function(results) {
+            console.log(results);
+            $('#search-results').empty();
+            $.each(
+              results.data,
+              function(index, result) {
+                $('#search-results').append('<img src="' + result.images.thumbnail.url + '">');
+              }
+            )
+          }
+        )
+      }
+    );
+  }
+);
